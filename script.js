377b2292580d6b0aecefcf3ac379ad1919e36fe8
An Pham <ppa.993@gmail.com>
2017-10-10 09:11:58 +0000

    zero global variable pattern

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-var createWorker = function() {
+(function() {
+    var createWorker = function() {
 
-    var task1 = function() {
-        console.log('doing job 1');
+        var task1 = function() {
+            console.log('doing job 1');
+        };
+        var task2 = function() {
+            console.log('doing job 2');
+        };
+
+        return {
+            job1: task1,
+            job2: task2
+        };
     };
-    var task2 = function() {
-        console.log('doing job 2');
-    };
 
-    return {
-        job1: task1,
-        job2: task2
-    };
-};
+    var worker = createWorker();
 
-var worker = createWorker();
-
-worker.job1();
-worker.job2();
+    worker.job1();
+    worker.job2();
+}());
