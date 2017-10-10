6c25e80bf20795c070e78a7f8f747dc23b6d654a
An Pham <ppa.993@gmail.com>
2017-10-10 09:02:52 +0000

    JS module pattern

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.write('Hello Angular');
+var createWorker = function() {
+
+    var task1 = function() {
+        console.log('doing job 1');
+    };
+    var task2 = function() {
+        console.log('doing job 2');
+    };
+
+    return {
+        job1: task1,
+        job2: task2
+    };
+};
+
+var worker = createWorker();
+
+worker.job1();
+worker.job2();
